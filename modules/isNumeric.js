dd5f337ac7a9e7b9372a23d1dd93749e83398b0f
Chandan-devs-tech <chandanguptabsb@gmail.com>
2023-06-23 12:28:54 +0530

    Update webpack

--- modules/isNumeric.js
+++
@@ -1,3 +0,0 @@
-const isNumeric = (value) => /^-?\d+$/.test(value);
-
-export default isNumeric;
--- src/index.js
+++ src/index.js
@@ -1,39 +1,21 @@
-import createScore from '../modules/createScore.js';
-import isNumeric from '../modules/isNumeric.js';
-import setToLocalStorage from '../modules/localStorage.js';
+import isNumeric from './modules/isNumeric.js';
+import postScore from './modules/post.js';
+import scoreBoard from './modules/scoreBoard.js';
 import './style.css';
 
 const nameInput = document.querySelector('.inputName');
 const scoreInput = document.querySelector('.inputScore');
-const scoreInputBtn = document.querySelector('.scoreInputBtn');
-const scoreBoard = document.querySelector('.scoreBoard');
-const alert = document.querySelector('.alert');
-const arrayOfScore = JSON.parse(localStorage.getItem('arrayOfScore')) || [];
+const form = document.querySelector('.scoreInput');
+const refreshBtn = document.querySelector('.refreshBtn');
 
-const addScore = () => {
-  if (nameInput.value) {
+form.addEventListener('submit', (e) => {
+  if (nameInput && scoreInput) {
     if (isNumeric(scoreInput.value)) {
-      const person = {
-        name: nameInput.value,
-        score: scoreInput.value,
-      };
-      scoreBoard.innerHTML = '';
-      arrayOfScore.unshift(person);
-      createScore(arrayOfScore, scoreBoard);
-      setToLocalStorage(arrayOfScore);
-      nameInput.value = '';
-      scoreInput.value = '';
-    } else {
-      alert.textContent = 'Score should be a number!';
-      alert.style.display = 'flex';
-      setTimeout(() => {
-        alert.style.display = 'none';
-      }, 1000);
-      scoreInput.value = '';
+      e.preventDefault();
+      postScore(nameInput.value, scoreInput.value);
+      form.reset();
     }
   }
-};
+});
 
-scoreInputBtn.addEventListener('click', addScore);
-
-window.addEventListener('DOMContentLoaded', createScore(arrayOfScore, scoreBoard));
+refreshBtn.addEventListener('click', scoreBoard);
